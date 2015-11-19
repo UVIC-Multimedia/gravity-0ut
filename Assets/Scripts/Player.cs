2d0daf74c6bb600d0ed4f0c4452a9396c5a99ceb
Gerard Reches Urbano <gerardreches@gmail.com>
2015-11-19 17:54:22 +0100

    Player script created. Basic jump void created.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,13 +3,28 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+	public float jumpForce = 500f;
+
+	private Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-	
+		rb = gameObject.GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			rb.isKinematic = false;
+			rb.AddForce(Camera.main.transform.forward * jumpForce);
+		}
+	}
+
+	void OnCollisionEnter(Collision collision) 
+	{
+		Debug.Log ("Hem col·lisionat amb: " + collision.gameObject.tag);
+		rb.isKinematic = true;
+		//if (collision.gameObject.tag == "wall")
+		Camera.main.transform.localRotation = Quaternion.Euler(collision.transform.up);
 	}
 }
---
+++ Assets/Scripts/pushBox.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pushBox : MonoBehaviour {
+
+	public float force = 1000.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown (KeyCode.P)) {
+			Vector3 dir = Vector3.Cross(gameObject.transform.up, gameObject.transform.forward);
+
+			gameObject.GetComponent<Rigidbody>().AddForce(dir*force);
+		}
+	}
+}
